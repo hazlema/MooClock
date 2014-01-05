85cad4c9bfcccc0ceaeca25933ff9e43b2599b7a
Matthew Hazlett <hazlema@gmail.com>
2014-01-05 01:35:42 -0500

    More code restructuring and UI Animation Fix

--- js/clock.js
+++ js/clock.js
@@ -6,8 +6,6 @@ var Clock = new Class({
 	// and the timer
 	//
 	initialize: function () {
-		this.initHover();
-		this.initClick();
 
 		// Load Modules
 		//
@@ -16,29 +14,31 @@ var Clock = new Class({
 			'Timer': new Timer()
 		};
 
-		// Set the loaded flag 
-		// Start the timer
+		// Wire the events and the hovers
+		// then load the first module and start the app
+		// but first let us pause for a word from our
+		// sponser
 		//
-		this.renderDisplay = true;
-		this.activateModule();
-
-		this.isRunning = this.tick.periodical(1000, this);
+		(function () {
+			this.initHover();
+			this.initClick();
+			this.activateModule();
+			this.tick.periodical(1000, this);
+		}.bind(this)).delay(2000);
 	},
-
+	
 	// Setup the click events
 	//
 	initClick: function () {
-		var self = this;
-
 		document.getElements('.commandButton').addEvent('click', function (e) {
 			e.stopPropagation();
 
-			if (!this.hasClass('active')) {
+			if (!e.target.hasClass('active')) {
 				document.getElements('div.commandButton').removeClass('active');
-				this.addClass('active');
-				self.activateModule(this.getAttribute('display-panel'));
+				e.target.addClass('active');
+				this.activateModule(e.target.getAttribute('display-panel'));
 			}
-		});
+		}.bind(this));
 	},
 
 	// Setup the hover events
@@ -58,6 +58,8 @@ var Clock = new Class({
 		});
 	},
 
+	// Called every second
+	//
 	tick: function () {
 
 		// Call all the tick functions in all the
@@ -72,51 +74,62 @@ var Clock = new Class({
 		// If we are updating the screen or waiting
 		// for an animation to finish
 		//
-		//if (this.renderDisplay) {
+		if (this.renderDisplay) {
 			document.id('content').set('html', this.modules.active.display(this.modules.active));
-		//}
+		}
 	},
 
+	// Fade out the div then load the new content
+	// and fade it back in. (Without stoping the timer)
+	//
 	activateModule: function (moduleName) {
-		var self = this,
-			fxfad = new Fx.Morph(document.id('content'), {
-				transition: 'quart:out',
-				duration: 500,
-				link: 'chain',
-				onStart: function () {
-					self.renderDisplay = false;
-				},
-				onComplete: function () {
-					if (typeof moduleName == 'undefined') {
-						self.modules.active = Object.values(self.modules)[0];
-					} else {
-						// Search for module
-						//
-						Object.each(self.modules, function (value, key) {
-							if (key == moduleName) {
-								self.modules.active = value;
-							}
-						});
+		var self = this;
+		
+		var start = function () {
+			self.renderDisplay = false;
+		};
+		
+		var complete = function () {
+			if (typeof moduleName == 'undefined') {
+				self.modules.active = Object.values(self.modules)[0];
+			} else {
+		
+				// Search for module
+				//
+				Object.each(self.modules, function (value, key) {
+					if (key == moduleName) {
+						self.modules.active = value;
 					}
+				});
+			}
 
-					// Update and wire modules commands
-					//
-					document.id('commands').set('html', self.modules.active.commands());
-					document.id("commands").getElements("a").addEvent('click', function (e, ele) {
-						self.modules.active.dispatch(this.innerHTML, self.modules.active);
-					});
-
-					self.modules.active.display(self.modules.active);
-					self.renderDisplay = true;
-					document.id('content').set('html', self.modules.active.display(self.modules.active));
-					document.id('content').fade('in');
-					console.log('Rendering: on');
-				}
+			// Update and wire modules commands
+			//
+			document.id('commands').set('html', self.modules.active.commands());
+			document.id("commands").getElements("a").addEvent('click', function (e, ele) {
+				self.modules.active.dispatch(this.innerHTML, self.modules.active);
 			});
 
-		fxfad.start({
-			'opacity': 0
+			self.modules.active.display(self.modules.active);
+			self.renderDisplay = true;
+			document.id('content').set('html', self.modules.active.display(self.modules.active));
+			
+			new Fx.Morph(document.id('content'), {
+				transition: 'Sine:in',
+				duration: 250,
+				link: 'chain'
+			}).start({'opacity': 1, 'font-size': '140px'});
+		};
+					
+		var fxFadeOut = new Fx.Morph(document.id('content'), {
+			transition: 'Sine:out',
+			duration: 250,
+			link: 'chain',
+			onStart: start,
+			onComplete: complete
 		});
+		
+		fxFadeOut.start({'opacity': 0, 'font-size': '50px'});
 	}
 });
 
